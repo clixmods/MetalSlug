723e670e510e83072a571c1800b3f5010587d505
Shixx <theo.carouge34@gmail.com>
2023-01-03 21:13:07 +0100

    Camera clamp

--- Assets/Scripts/Player/CameraMotor.cs
+++ Assets/Scripts/Player/CameraMotor.cs
@@ -11,6 +11,27 @@ public class CameraMotor : MonoBehaviour
     public float speed = 0.15f;
 
     private Vector3 desiredPosition;
+
+    // Clamp code
+
+    // the transform of the camera
+    public Transform cameraTransform;
+    // the left limit
+    public float leftBoundary;
+    // the right limit
+    public float rightBoundary;
+
+    private void Update()
+    {
+        // stock the pos of the camera in a vector3
+        Vector3 targetPosition = cameraTransform.position;
+
+        // clamp the camera position so it doesnt go beyond the left and right limits
+        targetPosition.x = Mathf.Clamp(targetPosition.x, leftBoundary, rightBoundary);
+
+        transform.position = targetPosition;
+    }
+
     private void LateUpdate()
     {
         Vector3 delta = Vector3.zero;
