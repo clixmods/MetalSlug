5fd6d3be1ec5575f93a4b636ee9b2642437cdb45
clixmods <clix.mods.cod@gmail.com>
2023-01-14 18:24:02 +0100

    Disable Game ui in the startup

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,11 @@ public class UIManager : MonoBehaviour
     {
         LevelManager.eventSessionStart += LevelManager_eventSessionStart;
         LevelManager.eventEndgame += LevelManager_eventEndgame;
+    }
+
+    private void Start()
+    {
+        gameObject.SetActive(false);
     }
 
     private void LevelManager_eventEndgame()
